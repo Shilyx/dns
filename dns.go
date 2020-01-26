e41fcee7d4fcb059a40bafafb89ef360a413c4e8
shilyx <shilyx@outlook.com>
2020-01-26 19:51:13 +0800

    修正dns库的一些参数，这些参数导致较大的payload无法正确接收

--- dns.go
+++ dns.go
@@ -7,9 +7,9 @@ const (
 	defaultTtl = 3600    // Default internal TTL.
 
 	// DefaultMsgSize is the standard default for messages larger than 512 bytes.
-	DefaultMsgSize = 4096
+	DefaultMsgSize = 14096
 	// MinMsgSize is the minimal size of a DNS packet.
-	MinMsgSize = 512
+	MinMsgSize = 60000
 	// MaxMsgSize is the largest possible DNS packet.
 	MaxMsgSize = 65535
 )
